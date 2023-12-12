285ae02c3d83c3e0b7eb390f7707cca214ab2ff6
Jaakko <jaakko.lipponen01@gmail.com>
2023-12-12 11:54:20 +0200

    T1

--- api/api.js
+++ api/api.js
@@ -3,36 +3,55 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 let users = [];
 
-function userchecker(username, users) {
-  // Iterate over the array of user objects
+function userchecker(username) {
   for (let user of users) {
-    // Check if the username matches the 'username' property of the current object
     if (user.username === username) {
-      return true; // Return true if the username is found
+      return true;
     }
   }
-  return false; // Return false if the username is not found
+  return false;
 }
 
 router.post("/user/register", async (req, res) => {
   const username = req.body.username;
 
   bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      res.status(500).send("Error generating salt");
+      return;
+    }
     bcrypt.hash(req.body.password, salt, function (err, hash) {
-      if (err) throw err;
+      if (err) {
+        res.status(500).send("Error hashing password");
+        return;
+      }
 
       if (!userchecker(username)) {
-        let user = JSON.parse({
-          id: users.length,
+        const user = {
+          id: users.length + 1,
           username: username,
           password: hash,
-        });
+        };
 
         users.push(user);
+        console.log(user);
         res.send(user);
       } else {
-        res.status(400).message("username check failed");
+        res.status(400).send("Username already exists");
       }
     });
   });
 });
+
+router.get("/user/list", (req, res) => {
+  res.json(users);
+});
+
+router.post(
+  "/user/login",
+  body("username").trim().escape(),
+  body("password").escape(),
+  async (req, res) => {}
+);
+
+module.exports = router;
